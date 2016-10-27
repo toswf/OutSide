5f5b9255fb52b53b293379b905fb20618801bcf5
niuhao <toswf@163.com>
2016-10-27 18:18:50 +0800

    我添加了jsonrpc4j的库,但是搞了两天后我发现，我们目前的项目不适合用java做服务端。 我计划明天改C++了，但是VS2015的安装真是蛋疼的不行。

--- src/main/java/com/toswf/controller/UserController.java
+++ src/main/java/com/toswf/controller/UserController.java
@@ -3,10 +3,18 @@ package com.toswf.controller;
 import com.toswf.db.DBConnection;
 import com.toswf.db.entities.UserEntity;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -15,8 +23,8 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
-    @RequestMapping(value = "test.do")
-    public String test(ModelMap map){
+    @RequestMapping(value = "testGet.do")
+    public String testGet(ModelMap map){
         Session session = DBConnection.getSession();
         List<UserEntity> users = session.createCriteria(UserEntity.class).list();
         map.addAttribute("users", users);
@@ -24,8 +32,33 @@ public class UserController {
         return "users";
     }
 
-    @RequestMapping(value = "add.do")
-    public boolean add(){
-
+    @RequestMapping(value = "testAdd.do")
+    public String testAdd(HttpServletRequest request, HttpServletResponse response) throws JSONException, IOException {
+        addUser();
+        return null;
     }
+
+    @RequestMapping(value = "register.do")
+    public String registerUser(HttpServletRequest request, HttpServletResponse response) throws JSONException, IOException{
+        return "";
+    }
+
+    private void addUser(){
+        Session session = DBConnection.getSession();
+        Transaction ta = session.beginTransaction();
+        UserEntity ue = new UserEntity();
+        ue.setAccount("testAccount");
+        ue.setGuid(1L);
+        ue.setNickname("toswf");
+        ue.setSex("1");
+        session.save(ue);
+        ta.commit();
+        session.close();
+    }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/toswf/util/View.java
@@ -0,0 +1,30 @@
+package com.toswf.util;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by niuhao on 2016/10/27.
+ */
+public class View {
+
+    public static String showJson(HttpServletRequest request, HttpServletResponse response, JSONObject ret) throws IOException{
+        if (ret == null){
+            return null;
+        }
+        //todo by niuhao;
+        return null;
+    }
+
+    public static String showJson(HttpServletResponse response, JSONObject ret) throws IOException{
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
+        response.getWriter().write(ret.toString());
+        return null;
+    }
+
+
+}
